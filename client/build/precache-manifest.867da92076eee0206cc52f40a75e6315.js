b6ad627fc857fc95bda0657be84b037819ce6160
Cvaniak <igna.cwaniak@gmail.com>
2020-02-27 19:39:48 +0100

    Init

---
+++ client/build/precache-manifest.867da92076eee0206cc52f40a75e6315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936435af1faeafca72082fe182917072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31dc23480d43db15e01d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e96a7c6fa251090746f3",
+    "url": "/static/js/2.9c4c7619.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9c4c7619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31dc23480d43db15e01d",
+    "url": "/static/js/main.551f9235.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
